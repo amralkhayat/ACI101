8b5a0fee7d5d3325bd85983d58bbc714a24d1a73
amr-Alkhayat <amralkhayat@gmail.com>
2019-11-12 09:40:30 +0200

    SAYHello

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = ""
+    var title:   String = "THERE"
     var content: String = "HELLO"
     
     var id: String = UUID().uuidString
